9648038c6d601513cc415bb22847a48b778398c8
sparsh_67 <43761860+sparsh-67@users.noreply.github.com>
2021-06-14 17:48:24 +0530

    implement 3 line version

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ const options=[
         "title": "interns-performance-report-june-2021.key"
     }
 ];
-let selectedItem=1;
-let charWidth=1;
-let charHeight=1;
-
+let selectedItem=0;
+let scaleFactor=1;
 //create util function for creating elements
 const createDomElement=(name,atrs,children)=>{
     let item=document.createElement(name);
@@ -48,42 +46,49 @@ const createPreview=(index,option)=>{
     Preview.appendChild(title);
     return Preview;
 }
-const getWidth=(str)=>{
-    document.getElementById("ruler");
-    ruler.innerHTML=str;
-    let width=ruler.offsetWidth;
-    ruler.innerHTML='';
-    return width;
-}
-const getTrucatedString=(option)=>{
-    let mid="...";
-    if(getWidth(option.title)<=300){return option.title;}
-    if(getWidth(mid)>=250)return mid;
-    let l=0,r=option.title.length-1;
-    let prefix='',suffix='';
-    while(l<=r){
-        prefix+=option.title[l];
-        l++;
-        if(getWidth(prefix+mid+suffix)>=250)break;
-        suffix=option.title[r]+suffix;
-        r--;
-        if(getWidth(prefix+mid+suffix)>=250)break;
-
-    }
-    return prefix+mid+suffix;
-}
 //create ListItem
 const createListItem=(index,option)=>{
-    
     const textBox=createDomElement("div",{class:"textBox"},[]);
-    let value=getTrucatedString(option);
-    textBox.innerHTML=value;
     const thumbnail=createDomElement("img",{class:"thumbnail",src:option.previewImage},[])
-    const thumbnailBox=createDomElement("div",{class:"thumbnailBox"},[thumbnail]);
-    const listItem=createDomElement("div",{style:`height:${6*charHeight}vh;`,class:`listItem ${index===selectedItem?'selected':''}`},[thumbnailBox,textBox]);
+    const thumbnailBox=createDomElement("div",{style:`width:${5*scaleFactor}vh;height:${5*scaleFactor}vh;`,class:"thumbnailBox"},[thumbnail]);
+    const listItem=createDomElement("div",{style:`width:${20*scaleFactor}vh;height:${6*scaleFactor}vh;`,class:`listItem ${index===selectedItem?'selected':''}`},[thumbnailBox,textBox]);
     return listItem;
 }
-
+let factor=1;
+// fill text after adjustment
+const fillText=(List,option,index)=>{
+    let currChild=List.childNodes[index].childNodes[1];
+            let l=0,r=option.title.length-1;
+            let prefix='',suffix='',mid='...';
+            let maxHeight=0;
+            currChild.innerHTML='M';
+            maxHeight=currChild.clientHeight;
+            currChild.innerHTML=option.title;
+            console.log(currChild.clientHeight,3*maxHeight);
+            if(currChild.clientHeight<=3*maxHeight){mid='';}
+                while(l<=r){
+                currChild.innerHTML=prefix+mid+suffix;
+                if(currChild.clientHeight>=4*maxHeight){
+                    break;
+                }
+                currChild.innerHTML=prefix+option.title[l]+mid+suffix;
+                if(currChild.clientHeight>=4*maxHeight){
+                    break;
+                }
+                prefix+=option.title[l];
+                l++;
+                if(l<=r){
+                    currChild.innerHTML=prefix+mid+option.title[r]+suffix;
+                    if(currChild.clientHeight>=4*maxHeight){
+                        break;
+                    }
+                    suffix=option.title[r]+suffix;
+                    r--;
+                }
+            }
+        List.childNodes[index].childNodes[1].innerHTML=prefix+mid+suffix;
+        
+}
 // create updateView function
 const updateView=(currItem)=>{
     selectedItem=currItem;
@@ -99,12 +104,14 @@ const List=document.querySelector(".list");
             Preview=createPreview(selectedItem,option);
         }
          let listItem=createListItem(index,option);
+         List.appendChild(listItem);
+         fillText(List,option,index);
          //handle click;
         listItem.addEventListener("click",(event)=>{
             event.preventDefault();
             updateView(index);
         })
-        List.appendChild(listItem);
+      
     })
 }
 
@@ -121,18 +128,18 @@ document.addEventListener("keydown",(event)=>{
         updateView(newIndex);
     }
 })
-//compute initial offSetHeight to scale data
-let ruler=document.getElementById("ruler");
-ruler.innerHTML="M";
-charHeight=ruler.offsetHeight/17;
-
 //Update dynamic
+let ruler=document.getElementById('ruler');
+ruler.innerHTML='M';
+scaleFactor=ruler.offsetHeight/17;
+ruler.innerHTML='';
+scaleFactor*=window.devicePixelRatio/2;
 window.addEventListener('resize',()=>{
-let ruler=document.getElementById("ruler");
-ruler.innerHTML="M";
-value = window.devicePixelRatio/2;
-charHeight=(ruler.offsetHeight/17)*value ;
-console.log("zooming!");
-updateView(selectedItem);
+    let ruler=document.getElementById('ruler');
+    ruler.innerHTML='M';
+    scaleFactor=ruler.offsetHeight/17;
+    ruler.innerHTML='';
+    scaleFactor*=window.devicePixelRatio/2;
+    updateView(selectedItem);
 });
 updateView(selectedItem);
